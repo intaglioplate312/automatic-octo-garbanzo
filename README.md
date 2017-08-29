01c587d592e50aadb5f11604b479f6cb13691a97
intaglioplate312 <helenelizabeth@me.com>
2017-08-29 07:12:21 -0500

    intial files

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# automatic-octo-garbanzo
-Seconday deploy for Northwestern Coding Bootcamp Second Project
+# Chicago Public Art App
+
+# What is the Chicago Public Art App?
+- The Chicago Public Art app is a web application which takes user submissions of public works of art and adds them to a database.
+- User submissions and database items are then displayed on an interactive map which details which works of public art are within the user's vicinity.
+
+# Setup
+- Run NPM install
+    + This will install the following node.js modules:
+        - Express.js
+        - Body-Parser
+        - Express-Handlebars
+        - MySql
+        - Method-Override
+        - Path-Exists
+        - File-System
+        - Express-FileUpload
+        - Detect-File
+
+# Using the Chicago Public Art App
+- Currently under construction. Check back soon!
---
+++ controllers/publicArt_controller.js
@@ -0,0 +1,102 @@
+var express = require("express");
+var detect = require('detect-file');
+var fs = require('fs');
+var artAction = require("../models/publicArt.js");
+var router = express.Router();
+var NodeGeocoder = require('node-geocoder');
+
+var options = {
+    provider: 'google',
+   
+    // Optional depending on the providers
+    httpAdapter: 'https', // Default
+    apiKey: 'AIzaSyCyrCSftGam0-rKNG6IQwobVMoo5j8QauY', // for Mapquest, OpenCage, Google Premier
+    formatter: null         // 'gpx', 'string', ...
+  };
+
+var geocoder = NodeGeocoder(options);
+
+
+router.get("/table", function(req,res){
+
+    artAction.all(function(data){
+        var artTableList = {
+            art:data
+        };
+        res.send(artTableList);
+    });
+    
+});
+
+router.get("/", function(req,res){
+        res.render("index");
+});
+
+router.get("/about", function(req,res){
+        res.render("about");
+});
+
+router.get("/api", function(req,res){
+        res.render("api");
+});
+    
+
+router.post("/upload", function(req, res){
+    var geoLat = '';
+    var geoLong = '';
+    var newArtDetails = {};
+    //file upload env setup
+    var imgFilePath = '';
+    let image = req.files.image;
+    var isImgPresent = detect('./public/assets/img/' + image.name);
+
+    if(!req.files){
+        return res.status(400).send('No files were uploaded.');
+    }
+    geocoder.geocode(req.body.address)
+    .then(function(res) {
+        geoLat = res[0].latitude;
+        geoLong = res[0].longitude;
+      imageUpload();
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
+    
+    function imageUpload() {
+        if (isImgPresent == null){
+            if (image.mimetype !== 'image/png' && image.mimetype !== 'image/jpeg'){
+                return res.send("You tried uploading a bad filetype.  Please upload PNG or JPEG files ONLY!")
+            }
+                imgFilePath = ('/assets/img/' + image.name);
+                image.mv('./public/assets/img/' + image.name, function(err){
+                    if (err){
+                        return res.status(500).send(err);
+                    }
+                })
+        } else {
+             res.send("file didn't upload because the same name exists on the server.  Please rename and try again.");
+             return;
+        }
+        newArtObject();
+        artAction.add(newArtDetails);
+        res.redirect('/');
+
+
+    }
+
+    function newArtObject(){
+        newArtDetails = {
+            artist: req.body.artist,
+            title: req.body.title,
+            description: req.body.description,
+            address: req.body.address,
+            lat: geoLat,
+            long: geoLong,
+            filePath: imgFilePath
+        }
+    }   
+ 
+});
+
+module.exports = router;
