7bb581c184d336f7a52e01bf81df417c370bc2c8
intaglioplate312 <helenelizabeth@me.com>
2017-08-29 07:37:46 -0500

    clean up schema and image folder

--- db/schema.sql
+++ db/schema.sql
@@ -16,21 +16,6 @@ CREATE database chicago_public;
 
 USE chicago_public;
 
-# Dump of table api
-# ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `user`;
-
-CREATE TABLE `user` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(250) NOT NULL DEFAULT '',
-  `email` varchar(250) NOT NULL DEFAULT '',
-  `password` varchar(15) NOT NULL DEFAULT '',
-  `login_name` varchar(15) NOT NULL DEFAULT '',
-  `timestamp` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-); 
-
 # Dump of table art
 # ------------------------------------------------------------
 
@@ -43,29 +28,11 @@ CREATE TABLE `art` (
   `media` varchar(250) DEFAULT NULL,
   `description` mediumtext,
   `viewable` tinyint(1) NOT NULL DEFAULT '0',
-  `img_file_path` varchar(250) DEFAULT NULL,
-  `geolat` float(10,6) DEFAULT NULL,
-  `geolng` float(10,6) DEFAULT NULL,
-  `google_geo` json NOT NULL,
-  `street_number` int(15) DEFAULT NULL,
-  `route` varchar(100) DEFAULT NULL,
-  `city` varchar(100) DEFAULT NULL,
-  `state` varchar(20) DEFAULT NULL,
-  `zip` int(6) DEFAULT NULL,
+  `path` varchar(250) DEFAULT NULL,
+  `lat` double unsigned zerofill DEFAULT NULL,
+  `long` double DEFAULT NULL,
+  `location` varchar(250) NOT NULL DEFAULT '',
   `user_id` int(11) DEFAULT NULL,
   `date` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-);
-
-# Dump of table user
-# ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `api`;
-
-CREATE TABLE `api` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `key` varchar(250) DEFAULT '',
-  `owner` varchar(250) DEFAULT NULL,
-  `url` varchar(250) DEFAULT NULL,
   PRIMARY KEY (`id`)
 );
